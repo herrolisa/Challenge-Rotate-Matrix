b77634e8eddc23067a614729b189ec70bdc5a231
Lisa <misslisazhou@gmail.com>
2016-06-06 20:45:25 -1000

    create function to rotate matrix

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -1,7 +1,7 @@
 /*  MatrixRotator(matrix)
  *
  *  @param matrix                        a multidimensional array containing the matrix
- *  
+ *
  *  @public property matrix              the matrix
  *
  *  @public method rotate(direction)     direction is either
@@ -13,13 +13,34 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
+
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
+  var newArr = [];
+  if (direction === Direction.CW){
+    for (var i = 0; i < this.matrix.length; i++) {
+      newArr.push([]);
+      for (var j = this.matrix.length - 1; j >= 0; j--) {
+        newArr[i].push(this.matrix[j][i]);
+      }
+    }
+  }else{
+    for (var k = 0; k < this.matrix.length; k++) {
+      newArr.push([]);
+      for (var l = 0; l < this.matrix.length; l++) {
+        newArr[k].unshift(this.matrix[l][k]);
+      }
+    }
+  }
+
+  console.log(newArr);
+  this.matrix = newArr;
+
+
 
   // must be a valid Direction, see Direction.js
 
